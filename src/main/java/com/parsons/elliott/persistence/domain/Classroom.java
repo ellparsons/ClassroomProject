fbb95641533069b98a07a237bb3187af18c0888f
ellparsons <elliottparsons13@gmail.com>
2019-02-01 11:03:52 +0000

    fixing conflict

--- src/main/java/com/parsons/elliott/persistence/domain/Classroom.java
+++ src/main/java/com/parsons/elliott/persistence/domain/Classroom.java
@@ -1,18 +1,5 @@
 package com.parsons.elliott.persistence.domain;
 
 public class Classroom {
-
-	@Entity
-	public class Classroom {
-		
-		@ID
-		@GeneratedValue(strategy=GenerationType.AUTO)
-		private Long classroomId;
-		
-		private String
-		
-		private String
-		
-	}
 	
 }
